ea5e16790a72f1de448544af66b2511bb7493b97
Francisco Mejía <fmejiaantezano@gmail.com>
2022-05-22 17:43:50 -0500

    update

--- main.cpp
+++ main.cpp
@@ -129,23 +129,24 @@ void escenario()
 
 int main(int argc, char **argv)
 {
-    vec3 lookFrom(50.0f, 20.0f, 10.0f);
-    vec3 lookAt(0.0f, 0.0f, 0.0f);
-    vec3 up(0.0f, 1.0f, 0.0f);
+    escenario();
+    // vec3 lookFrom(50.0f, 20.0f, 10.0f);
+    // vec3 lookAt(0.0f, 0.0f, 0.0f);
+    // vec3 up(0.0f, 1.0f, 0.0f);
 
-    Camera camera(60.0f, 10.0f, 800, 600, lookFrom, lookAt, up);
-    camera.calcular_vectores();
+    // Camera camera(60.0f, 10.0f, 800, 600, lookFrom, lookAt, up);
+    // camera.calcular_vectores();
 
-    std::cout << "xe: " << camera.xe << "\n"
-              << "ye: " << camera.ye << "\n"
-              << "ze: " << camera.ze << std::endl;
+    // std::cout << "xe: " << camera.xe << "\n"
+    //           << "ye: " << camera.ye << "\n"
+    //           << "ze: " << camera.ze << std::endl;
 
-    Sphere *sphere = new Sphere(vec3(0.0f, 0.0f, 0.0f), 4.0f);
+    // Sphere *sphere = new Sphere(vec3(0.0f, 0.0f, 0.0f), 4.0f);
 
-    Ray ray;
-    ray.direction = vec3(0.0f, 0.0f, 0.0f);
-    ray.origin = vec3(40.0f, 40.0f, 40.0f);
+    // Ray ray;
+    // ray.direction = vec3(0.0f, 0.0f, 0.0f);
+    // ray.origin = vec3(40.0f, 40.0f, 40.0f);
 
-    vec3 pi = sphere->get_pi(ray);
-    std::cout << pi << std::endl;
+    // vec3 pi = sphere->get_pi(ray);
+    // std::cout << pi << std::endl;
 }
